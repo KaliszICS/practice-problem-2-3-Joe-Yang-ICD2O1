31d68f586085c9a29a066474ed25c2ecea04c6f5
Joe-Yang-ICD2O1 <joe.yang@student.tdsb.on.ca>
2024-10-16 14:16:48 +0000

    answer

--- PP2_3.py
+++ PP2_3.py
@@ -3,12 +3,12 @@
 def q1(): 
   #Write Assignment code here
   num = input("In: ")
-  if num[-1:] == "y":
-    print("-ies")
-  elif num[-2:] == "ey":
-    print("-eys")
-  elif num[-3:] == "ife":
+  if num[-3:] == "ife":
     print("-ives")
+  elif num[-2:] == "ey":
+    print("-eys")  
+  elif num[-1:] == "y":
+    print("-ies") 
   else: 
     print("-s")
 
@@ -16,11 +16,11 @@ def q1():
 def q2(): 
   #Write Assignment code here
 
-  num = input("In: ")
-  if num < 0:
-    print(f"{num} is negative")
-  elif num > 0:
+  num = int(input("In: "))
+  if num > 0:
     print(f"{num} is positive")
+  elif num < 0:
+    print(f"{num} is negative")
 
 def q3(): 
   #Write Assignment code here
@@ -28,19 +28,19 @@ def q3():
   num = float(input("Input a number: "))
   num2 = float(input("Input a number: "))
   num3 = float(input("Input a number: "))
-  if num != num2 and num != num3 and num2 != num3:
-    print("Scalene")
-  elif num == num2 and num != num3 and num2 != num3:
-    print("Isosceles")
-  elif num == num2 and num == num3 and num2 == num3:
-    print("Equilateral")
-  elif num + num2 < num3:
+  if num + num2 < num3:
     print("No Triangle")
   elif num + num3 < num2:
     print("No Triangle")
   elif num2 + num3 < num:
     print("No Triangle")
-
+  elif num != num2 and num != num3 and num2 != num3:
+    print("Scalene")
+  elif num == num2 and num != num3 and num2 != num3:
+    print("Isosceles")
+  elif num == num2 and num == num3 and num2 == num3:
+    print("Equilateral")
+  
 
 
 
