527fee44c6df9d05821d2a81b006c9e2966d25ed
Joe-Yang-ICD2O1 <joe.yang@student.tdsb.on.ca>
2024-10-16 14:00:38 +0000

    answer

--- PP2_3.py
+++ PP2_3.py
@@ -2,10 +2,48 @@
 
 def q1(): 
   #Write Assignment code here
+  num = input()
+  if num[-1] == "y"
+    print("ies")
+  elif num[-1] == "ey":
+    print("eys")
+  elif num[-1] == "if"
+    print("ives")
+  else: 
+    print("s")
 
 
 def q2(): 
   #Write Assignment code here
+
+  num = input()
+  if num < 0:
+    print(f"{num} is negative")
+  elif num > 0:
+    print(f"{num} is positive")
+
+def q3(): 
+  #Write Assignment code here
+
+  num = float(input("Input a number: "))
+  num2 = float(input("Input a number: "))
+  num3 = float(input("Input a number: "))
+  if num != num2 and num != num3 and num2 != num3:
+    print("Scalene")
+  elif num == num2 and num != num3 and num2 != num3:
+    print("Isosceles")
+  elif num == num2 and num == num3 and num2 == num3:
+    print("Equilateral")
+  elif num + num2 < num3:
+    print("No Triangle")
+  elif num + num3 < num2:
+    print("No Triangle")
+  elif num2 + num3 < num:
+    print("No Triangle")
+
+
+
+
 
 
 
